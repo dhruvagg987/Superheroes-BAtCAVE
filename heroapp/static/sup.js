21b1899018b6a2b8bb4cf0910794fde01b2f9d94
dhruv aggarwal <dhruv.1822it1051@kiet.edu>
2020-05-25 16:37:20 +0530

    adding_html_using_JS

--- heroapp/static/sup.js
+++ heroapp/static/sup.js
@@ -6,9 +6,21 @@ function jinga(){
         dataType: 'json',
         url: "https://cors-anywhere.herokuapp.com/superheroapi.com/api/2620686038220622/search/"+str,
         headers: { Origin:"localhost", },
-        success: function (cont) {
-            $("body").append(cont.results[0].id);
-            console.log(cont);
+        success: function (response) {
+            // $("body").append(response.results.id);
+            // alert(response);
+            console.log(response);
+            const nwimg=document.createElement('img');
+            nwimg.src=response.results[0].image.url;
+            document.getElementById('gallery').appendChild(nwimg);
+            console.log("tries....");
+            console.log(response.results);
+            for(let i=0;i<response.results.length;i++)
+            {
+                var str='<div class="col-lg-4 mb-4"><div class="card "><img class="card-image-top" src="'+response.results[i].image.url+'" ><div class="card-body"><h5 class="card-title">'+response.results[i].name+'</h5><p class="card-text">'+response.results[i]+'</p></div></div></div>';
+                $("#newbox").append(str);
+                console.log(response.results[i]);
+            }
             console.log("hi..its working");                
         },
         Error: function(){
